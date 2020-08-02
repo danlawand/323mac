a4950d1a71e227f9c5f01f3bdce6aca7fb4d0615
dan.lawand <daniel.lawand@usp.br>
2020-08-02 14:53:05 -0300

    pos-trancado

--- 0ep (cópia)/Celula.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "Celula.h"
-#include "Aviao.h"
-/*Construtor: Que será usado na Fila.cpp*/
-Celula::Celula(Aviao *a){aeronave = a;}
-/*Destrutor*/
-Celula::~Celula() {}
